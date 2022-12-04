40ebdbca4ffd5336766648970b841011fc9c60ac
Monica Zarate <monicazaratecortes@gmail.com>
2022-12-04 00:30:06 -0800

    Hamburger Menu for mobile implementation.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
 // Vendor imports
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { slide as Menu } from "react-burger-menu";
 
 // Project imports
 import logo from "../assets/imgs/logo.svg";
 
 function Header() {
+
+    const [isNavVisible, setIsNavVisible] = useState(false);
+    const [matches, setMatches] = useState(window.matchMedia("(max-width: 767px)").matches);
+
+    useEffect(() => {
+        window.matchMedia("(max-width: 767px)").addEventListener('change', e => setMatches(e.matches));
+    }, []);
+
+    const closeNav = () => {
+        setIsNavVisible(false);
+    };
+
+    const handleStateChange = (state) => {
+        setIsNavVisible(state.isOpen);
+    };
 
     return (
         <header className="header">
@@ -20,6 +36,11 @@ function Header() {
                         <li className="header__nav-li"><NavLink to="/about" title="About" className={({isActive}) => isActive ? "header__link--active" : "header__link"}>about</NavLink></li>
                     </ul>
                 </nav>
+                {matches && <Menu className="header__mobile" right width="50%" noOverlay isOpen={isNavVisible} onStateChange={(state) => handleStateChange(state)}>
+                    <Link to="/design" title="Design" className="header__mobile" onClick={closeNav}>design</Link>
+                    <Link to="/development" title="Development" className="header__mobile" onClick={closeNav}>development</Link>
+                    <Link to="/about" title="About" className="header__mobile" onClick={closeNav}>about</Link>
+                </Menu>}
             </div>
         </header>
     )
