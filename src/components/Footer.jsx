5fcd3765d34d1b0058813e5a5dd472b45aed7ffe
Monica Zarate <60247850+monica-zarate@users.noreply.github.com>
2023-03-24 14:00:37 -0700

    Implemented React Responsive library. Created a SocialIcons Component. WIP.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,22 @@
 // Vendor imports
 import React from "react";
+import { useMediaQuery } from 'react-responsive';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { motion as m } from "framer-motion";
+// Project imports
+import SocialIcons from "./SocialIcons";
 
 function Footer() {
 
     let year = new Date().getFullYear();
+
+    const isMobile = useMediaQuery({query: '(max-width: 767px)'});
 
     return (
         <footer className="footer">
             <ul className="flex-c footer__wrapper">
                 <p>© Monica Zarate {year}</p>
                 <p>Designed and developed with 🤍 <br/> in Vancouver, BC</p>
-                <ul className='flex-c footer__social'>
-                    <m.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} style={{ x: 0 }} transition={{ease: 'easeInOut'}}><a href="https://www.linkedin.com/in/monica-zarate/" title="Monica Zarate's LinkedIn Profile" target="_blank"><FontAwesomeIcon icon={faLinkedin}/></a></m.li>
-                    <m.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} style={{ x: 0 }} transition={{ease: 'easeInOut'}}><a href="https://github.com/monica-zarate" title="Monica Zarate's Github Profile" target="_blank"><FontAwesomeIcon icon={faGithub}/></a></m.li>
-                    <m.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} style={{ x: 0 }} transition={{ease: 'easeInOut'}}><a href="mailto:hello@monicazarate.com" title="Send me an email: hello@monicazarate.com" target="_blank"><FontAwesomeIcon icon={faEnvelope}/></a></m.li>
-                </ul>
+                {isMobile ? SocialIcons('mobile', 'footer') : SocialIcons('tablet', 'footer')}
             </ul>
       </footer>
     )
---
+++ src/components/SocialIcons.jsx
@@ -0,0 +1,49 @@
+// Vendor imports
+import React from "react";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { motion as m } from "framer-motion";
+
+export default function SocialIcons(device, component) {
+
+    const linkedInIcon = (<a href="https://www.linkedin.com/in/monica-zarate/" title="Monica Zarate's LinkedIn Profile" target="_blank"><FontAwesomeIcon icon={faLinkedin}/></a>);
+
+    const gitHubIcon = (<a href="https://github.com/monica-zarate" title="Monica Zarate's Github Profile" target="_blank"><FontAwesomeIcon icon={faGithub}/></a>);
+
+    const emailIcon = (<a href="mailto:hello@monicazarate.com" title="hello@monicazarate.com" target="_blank"><FontAwesomeIcon icon={faEnvelope}/></a>);
+
+    let socialLinks = '';
+
+    switch (device) {
+        case 'mobile':
+            socialLinks = (<ul className={`flex-c ${component}__social`}>
+                <li>
+                    {linkedInIcon}
+                </li>
+                <li>
+                    {gitHubIcon}
+                </li>
+                <li>
+                    {emailIcon}
+                </li>
+            </ul>);
+            break;
+        case 'tablet': 
+            socialLinks = (<ul className={`flex-c ${component}__social`}>
+                <m.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} style={{ x: 0 }} transition={{ease: 'easeInOut'}}>
+                    {linkedInIcon}
+                </m.li>
+                <m.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} style={{ x: 0 }} transition={{ease: 'easeInOut'}}>
+                    {gitHubIcon}
+                </m.li>
+                <m.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} style={{ x: 0 }} transition={{ease: 'easeInOut'}}>
+                    {emailIcon}
+                </m.li>
+            </ul>);
+            break;
+    }
+
+    return socialLinks;
+}
